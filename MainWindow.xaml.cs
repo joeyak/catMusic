19aa8b2e96a985a5893078ec07014ec1cb552846
joeyak <joeyak97@gmail.com>
2019-08-08 23:13:10 -0400

    Add code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace catMusic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int _count = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Music.MediaEnded += (o, e) =>
+            {
+                IncrementLoop();
+                Music.Position = new TimeSpan();
+                Music.Play();
+            };
+
+            FilePickerButton.Click += (o, e) =>
+            {
+                var ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() ?? false)
+                {
+                    Music.Source = new Uri(ofd.FileName);
+                    FileName.Content = $"{ofd.SafeFileName}";
+
+                    StartButton.IsEnabled = true;
+                }
+            };
+
+            StartButton.Click += (o, e) =>
+            {
+                Music.Play();
+                IncrementLoop();
+                StartButton.IsEnabled = false;
+                StopButton.IsEnabled = true;
+            };
+
+            StopButton.Click += (o, e) =>
+            {
+                Music.Stop();
+                StartButton.IsEnabled = true;
+                StopButton.IsEnabled = false;
+            };
+        }
+
+        void IncrementLoop()
+        {
+            _count++;
+            Counter.Content = $"Loop Count: {_count}";
+            File.WriteAllText("count.txt", _count.ToString());
+        }
+    }
+}
